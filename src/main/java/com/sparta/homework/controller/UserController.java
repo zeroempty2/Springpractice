f3ed5aafebbe19b5ed825f7e118c222b335bca84
leeyoungbin <2zeroempty@gmail.com>
2022-12-16 12:03:19 +0900

    "Response Entity를 사용하여 상태메세지 반환하도록 수정"

--- src/main/java/com/sparta/homework/controller/UserController.java
+++ src/main/java/com/sparta/homework/controller/UserController.java
@@ -2,29 +2,40 @@ package com.sparta.homework.controller;
 
 import com.sparta.homework.dto.LoginRequestDto;
 import com.sparta.homework.dto.SignupRequestDto;
+import com.sparta.homework.responseMessageData.Message;
+import com.sparta.homework.responseMessageData.StatusEnum;
 import com.sparta.homework.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
 public class UserController {
 
     private final UserService userService;
+    private final Message message;
+
 
     @PostMapping("/signup")
-    public String signup(@RequestBody SignupRequestDto signupRequestDto) {
+    public ResponseEntity<Message> signup(@Valid @RequestBody SignupRequestDto signupRequestDto) {
         userService.signup(signupRequestDto);
-        return "생성완료";
+        message.setStatus(StatusEnum.OK);
+        message.setMessage("생성 완료");
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
-
+    @ResponseBody
     @PostMapping("/login")
-    public String login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
+    public ResponseEntity<Message> login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
         userService.login(loginRequestDto, response);
-        return "success";
+        message.setStatus(StatusEnum.OK);
+        message.setMessage("로그인 성공");
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
 }
