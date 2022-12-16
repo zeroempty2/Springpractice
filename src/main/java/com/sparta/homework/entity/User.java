9a5c128cf03288ac85d0ce5148b224220287a377
leeyoungbin <2zeroempty@gmail.com>
2022-12-16 12:02:24 +0900

    "username과 password에 특정문자만 입력하로독 Pattern 추가."

--- src/main/java/com/sparta/homework/entity/User.java
+++ src/main/java/com/sparta/homework/entity/User.java
@@ -1,15 +1,10 @@
 package com.sparta.homework.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.BatchSize;
-import org.springframework.boot.convert.DataSizeUnit;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -19,9 +14,11 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @Size(min = 4, max = 10)
+    @Pattern(regexp = "^[0-9a-z]*$",message = "최소 4자, 최대 10자 영어소문자와 숫자만 입력할 수 있습니다")
     @Column(nullable = false)
     private String username;
     @Size(min = 8, max = 15)
+    @Pattern(regexp = "^[0-9a-zA-z]*$",message = "최소 8자, 최대15자 영어와 숫자만 입력할 수 있습니다")
     @Column(nullable = false)
     private String password;
 
