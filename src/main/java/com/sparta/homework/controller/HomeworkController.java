6ff48c2b29dc63fcad9b93e85a5e9ed9b0044253
leeyoungbin <2zeroempty@gmail.com>
2022-12-16 12:06:10 +0900

    "게시글 생성과 업데이트시 게시글 반환, 삭제시 ResponseEntity를 통해 상태메시지를 반환하도록 수정"

--- src/main/java/com/sparta/homework/controller/HomeworkController.java
+++ src/main/java/com/sparta/homework/controller/HomeworkController.java
@@ -3,10 +3,13 @@ package com.sparta.homework.controller;
 import com.sparta.homework.dto.HomeworkRequestDto;
 import com.sparta.homework.dto.HomeworkResponseByIdDto;
 import com.sparta.homework.dto.HomeworkResponseDto;
-import com.sparta.homework.entity.Homework;
+import com.sparta.homework.responseMessageData.Message;
+import com.sparta.homework.responseMessageData.StatusEnum;
 import com.sparta.homework.service.HomeworkService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,9 +19,10 @@ import java.util.List;
 @RequestMapping("/api")
 public class HomeworkController {
     private final HomeworkService homeworkService;
+    private final Message message;
 
     @PostMapping("/post")
-    public String createHomework(@RequestBody HomeworkRequestDto requestDto, HttpServletRequest request){
+    public HomeworkRequestDto createHomework(@RequestBody HomeworkRequestDto requestDto, HttpServletRequest request){
         return homeworkService.createHomework(requestDto,request);
     }
     @GetMapping("/posts")
@@ -30,12 +34,14 @@ public class HomeworkController {
        return homeworkService.getSelectHomeworks(id);
     }
     @PutMapping("/post/{id}")
-    public String updateHomework(@PathVariable Long id, @RequestBody HomeworkRequestDto requestDto, HttpServletRequest request){
+    public HomeworkRequestDto updateHomework(@PathVariable Long id, @RequestBody HomeworkRequestDto requestDto, HttpServletRequest request){
         return homeworkService.update(id,requestDto,request);
     }
     @DeleteMapping("/post/{id}")
-    public String deleteHomework(@PathVariable Long id,HttpServletRequest request){
-        return homeworkService.delete(id,request);
+    public ResponseEntity<Message> deleteHomework(@PathVariable Long id, HttpServletRequest request){
+        homeworkService.delete(id,request);
+        message.setStatus(StatusEnum.OK);
+        message.setMessage("삭제 완료");
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
-
 }
