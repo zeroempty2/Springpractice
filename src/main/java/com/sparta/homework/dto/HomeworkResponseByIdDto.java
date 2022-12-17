0b6996dcde313b4c2d3fd52b910d9ec1abf52769
leeyoungbin <2zeroempty@gmail.com>
2022-12-17 23:53:17 +0900

    "DTO Comment List도 같이 반환"

--- src/main/java/com/sparta/homework/dto/HomeworkResponseByIdDto.java
+++ src/main/java/com/sparta/homework/dto/HomeworkResponseByIdDto.java
@@ -1,8 +1,10 @@
 package com.sparta.homework.dto;
 
+import com.sparta.homework.repository.mapping.Comments;
 import lombok.Getter;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 public class HomeworkResponseByIdDto {
@@ -10,11 +12,13 @@ public class HomeworkResponseByIdDto {
     private String title;
     private LocalDate createdAt;
     private String username;
+    private List<Comments> comments;
 
-    public HomeworkResponseByIdDto(String title, String contents, LocalDate createdAt, String username) {
+    public HomeworkResponseByIdDto(String title, String contents, LocalDate createdAt, String username, List<Comments> comments) {
         this.title = title;
         this.contents = contents;
         this.createdAt = createdAt;
         this.username = username;
+        this.comments = comments;
     }
 }
