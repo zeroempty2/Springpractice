e4f2bccfad58ad779ce5ef120d6022a35f474681
leeyoungbin <2zeroempty@gmail.com>
2022-12-18 23:50:59 +0900

    "쓰이지 않는 구문 제거"

--- src/main/java/com/sparta/homework/repository/HomeworkRepository.java
+++ src/main/java/com/sparta/homework/repository/HomeworkRepository.java
@@ -4,10 +4,9 @@ import com.sparta.homework.entity.Homework;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
+
 
 public interface HomeworkRepository extends JpaRepository<Homework, Long> {
     List<Homework> findAllByOrderByCreatedAtDesc();
 
-    Optional<Homework> findByIdAndUserId(Long id, Long userId);
 }
