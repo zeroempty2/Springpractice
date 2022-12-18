c6442ba0d45ea9bb0fe0aaf14826295d5df3b64c
leeyoungbin <2zeroempty@gmail.com>
2022-12-18 23:57:42 +0900

    "저장기능 변경, 수정, 삭제 추가"

--- src/main/java/com/sparta/homework/service/CommentService.java
+++ src/main/java/com/sparta/homework/service/CommentService.java
@@ -14,6 +14,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static com.sparta.homework.entity.UserRoleEnum.ADMIN;
+
 
 @Service
 @RequiredArgsConstructor
@@ -46,12 +48,82 @@ public class CommentService {
                     () -> new IllegalArgumentException("게시글이 존재하지 않습니다")
             );
 
-            Comment comment = new Comment(requestDto,user,user.getUsername());
-            homework.addComments(comment);
-            commentRepository.save(comment);
+            Comment comment = new Comment(requestDto,user,user.getUsername(),homework);
+            commentRepository.saveAndFlush(comment);
             return requestDto;
         }
         throw new IllegalArgumentException("로그인 해 주십시오");
+    }
+
+    @Transactional
+    public CommentRequestDto updateComment(Long commentId, Long id, CommentRequestDto requestDto, HttpServletRequest request) {
+        String token = jwtUtil.resolveToken(request);
+        Claims claims;
+
+        if (token != null) {
+            if (jwtUtil.validateToken(token)) {
+                claims = jwtUtil.getUserInfoFromToken(token);
+            } else {
+                throw new IllegalArgumentException("유효한 토큰이 아닙니다.");
+            }
+
+            User user = userRepository.findByUsername(claims.getSubject()).orElseThrow(
+                    () -> new IllegalArgumentException("사용자가 존재하지 않습니다."));
+
+            homeworkRepository.findById(id).orElseThrow(
+                    () -> new NullPointerException("유효한 계정이 아니거나 게시글이 존재하지 않습니다.")
+            );
+
+
+            Comment comment = commentRepository.findById(commentId).orElseThrow(
+                    () -> new NullPointerException("유효한 계정이 아니거나 코멘트가 존재하지 않습니다.")
+            );
+
+            if(user.getRole().equals(ADMIN)){
+                comment.update(requestDto);
+                return requestDto;
+            }
+            else if (comment.getUser().getId().equals(user.getId())) {
+                comment.update(requestDto);
+                return requestDto;
+            } else {
+                throw new IllegalArgumentException("유효한 계정이 아닙니다");
+            }
+        }
+        throw new IllegalArgumentException("로그인 해 주십시오");
+    }
+    @Transactional
+    public void deleteComment(Long commentId, Long id, HttpServletRequest request) {
+        String token = jwtUtil.resolveToken(request);
+        Claims claims;
+
+        if (token != null) {
+            if (jwtUtil.validateToken(token)) {
+                claims = jwtUtil.getUserInfoFromToken(token);
+            } else {
+                throw new IllegalArgumentException("유효한 토큰이 아닙니다.");
+            }
+
+            User user = userRepository.findByUsername(claims.getSubject()).orElseThrow(
+                    () -> new IllegalArgumentException("사용자가 존재하지 않습니다."));
+
+            homeworkRepository.findById(id).orElseThrow(
+                    () -> new NullPointerException("유효한 계정이 아니거나 게시글이 존재하지 않습니다.")
+            );
+
+            Comment comment = commentRepository.findByIdAndHomework_Id(commentId,id).orElseThrow(
+                    () -> new NullPointerException("유효한 계정이 아니거나 코멘트가 존재하지 않습니다.")
+            );
+            System.out.println(comment);
+            if(user.getRole().equals(ADMIN)){
+                commentRepository.delete(comment);
+            }
+            else if (comment.getUser().getId().equals(user.getId())) {
+                commentRepository.delete(comment);
+            } else {
+                throw new IllegalArgumentException("유효한 계정이 아닙니다");
+            }
+        }
     }
 }
 
