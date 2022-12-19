2b90189bc1383c9d62e7093850afe75e9f86424a
leeyoungbin <2zeroempty@gmail.com>
2022-12-19 23:20:26 +0900

    "정규표현식 변경"

--- src/main/java/com/sparta/homework/entity/User.java
+++ src/main/java/com/sparta/homework/entity/User.java
@@ -17,7 +17,7 @@ public class User {
     @Pattern(regexp = "^[0-9a-z].{4,10}$",message = "최소 4자, 최대 10자 영어소문자와 숫자만 입력할 수 있습니다")
     @Column(nullable = false)
     private String username;
-    @Pattern(regexp = "^[^\\t\\r\\n\\v\\f].{8,15}$",message = "최소 8자, 최대15자 공백은 입력 할 수 없습니다")
+    @Pattern(regexp = "^[A-Za-z0-9#?!@$%^&*-].{8,15}$",message = "최소 8자, 최대15자 영어 대소문자, 특수문자를 입력할 수 있습니다.")
     @Column(nullable = false)
     private String password;
 
