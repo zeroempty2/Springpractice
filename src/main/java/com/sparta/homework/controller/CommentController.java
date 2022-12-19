be97e2e7eb14cdcd8ff6a8b674239bb0d6cd5c70
leeyoungbin <2zeroempty@gmail.com>
2022-12-19 11:20:24 +0900

    "jwt토큰 검증을 controller에서 하게 변경"

--- src/main/java/com/sparta/homework/controller/CommentController.java
+++ src/main/java/com/sparta/homework/controller/CommentController.java
@@ -1,10 +1,12 @@
 package com.sparta.homework.controller;
 
 import com.sparta.homework.dto.CommentRequestDto;
+import com.sparta.homework.jwt.JwtUtil;
 import com.sparta.homework.responseMessageData.DefaultRes;
 import com.sparta.homework.responseMessageData.ResponseMessage;
 import com.sparta.homework.responseMessageData.StatusCode;
 import com.sparta.homework.service.CommentService;
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,21 +18,54 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/post/{id}")
 public class CommentController {
     private final CommentService commentService;
-
+    private final JwtUtil jwtUtil;
     private final DefaultRes defaultRes;
     @PostMapping("/comment")
-    public CommentRequestDto addComment(@RequestBody CommentRequestDto requestDto, HttpServletRequest request, @PathVariable Long id){
-        return commentService.addComment(requestDto,request,id);
+    public CommentRequestDto addComment(@RequestBody CommentRequestDto requestDto, HttpServletRequest request, @PathVariable Long id) {
+        String token = jwtUtil.resolveToken(request);
+        Claims claims;
+        if (token != null) {
+            if (jwtUtil.validateToken(token)) {
+                claims = jwtUtil.getUserInfoFromToken(token);
+            } else {
+                throw new IllegalArgumentException("유효한 토큰이 아닙니다.");
+            }
+            String userNameToken = claims.getSubject();
+            return commentService.addComment(requestDto, userNameToken, id);
+        }
+        throw new IllegalArgumentException("로그인 해 주십시오");
     }
     @PutMapping("/comment/{commentId}")
     public CommentRequestDto updateComment(@RequestBody CommentRequestDto requestDto, HttpServletRequest request, @PathVariable Long id, @PathVariable Long commentId){
-        return commentService.updateComment(commentId,id,requestDto,request);
+        String token = jwtUtil.resolveToken(request);
+        Claims claims;
+        if (token != null) {
+            if (jwtUtil.validateToken(token)) {
+                claims = jwtUtil.getUserInfoFromToken(token);
+            } else {
+                throw new IllegalArgumentException("유효한 토큰이 아닙니다.");
+            }
+            String userNameToken = claims.getSubject();
+            return commentService.updateComment(commentId,id,requestDto,userNameToken);
+        }
+        throw new IllegalArgumentException("로그인 해 주십시오");
     }
     @DeleteMapping("/comment/{commentId}")
     public ResponseEntity<DefaultRes> deleteComment(HttpServletRequest request, @PathVariable Long id, @PathVariable Long commentId){
-        commentService.deleteComment(commentId,id,request);
-        defaultRes.setStatusCode(StatusCode.OK);
-        defaultRes.setResponseMessage(ResponseMessage.DELETE_SUCCESS);
-        return new ResponseEntity<>(defaultRes, HttpStatus.OK);
+        String token = jwtUtil.resolveToken(request);
+        Claims claims;
+        if (token != null) {
+            if (jwtUtil.validateToken(token)) {
+                claims = jwtUtil.getUserInfoFromToken(token);
+            } else {
+                throw new IllegalArgumentException("유효한 토큰이 아닙니다.");
+            }
+            String userNameToken = claims.getSubject();
+            commentService.deleteComment(commentId,id,userNameToken);
+            defaultRes.setStatusCode(StatusCode.OK);
+            defaultRes.setResponseMessage(ResponseMessage.DELETE_SUCCESS);
+            return new ResponseEntity<>(defaultRes, HttpStatus.OK);
+        }
+        throw new IllegalArgumentException("로그인 해 주십시오");
     }
 }
--- src/main/java/com/sparta/homework/controller/HomeworkController.java
+++ src/main/java/com/sparta/homework/controller/HomeworkController.java
@@ -3,10 +3,12 @@ package com.sparta.homework.controller;
 import com.sparta.homework.dto.HomeworkRequestDto;
 import com.sparta.homework.dto.HomeworkResponseByIdDto;
 import com.sparta.homework.dto.HomeworkResponseDto;
+import com.sparta.homework.jwt.JwtUtil;
 import com.sparta.homework.responseMessageData.DefaultRes;
 import com.sparta.homework.responseMessageData.ResponseMessage;
 import com.sparta.homework.responseMessageData.StatusCode;
 import com.sparta.homework.service.HomeworkService;
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -21,10 +23,22 @@ import java.util.List;
 public class HomeworkController {
     private final HomeworkService homeworkService;
     private final DefaultRes defaultRes;
+    private final JwtUtil jwtUtil;
 
     @PostMapping("/post")
-    public HomeworkRequestDto createHomework(@RequestBody HomeworkRequestDto requestDto, HttpServletRequest request){
-        return homeworkService.createHomework(requestDto,request);
+    public HomeworkRequestDto createHomework(@RequestBody HomeworkRequestDto requestDto, HttpServletRequest request) {
+        String token = jwtUtil.resolveToken(request);
+        Claims claims;
+        if (token != null) {
+            if (jwtUtil.validateToken(token)) {
+                claims = jwtUtil.getUserInfoFromToken(token);
+            } else {
+                throw new IllegalArgumentException("유효한 토큰이 아닙니다.");
+            }
+            String userNameToken = claims.getSubject();
+            return homeworkService.createHomework(requestDto, userNameToken);
+        }
+        throw new IllegalArgumentException("로그인 해 주십시오");
     }
     @GetMapping("/posts")
     public List<HomeworkResponseDto> getHomeworks(){
@@ -36,13 +50,37 @@ public class HomeworkController {
     }
     @PutMapping("/post/{id}")
     public HomeworkRequestDto updateHomework(@PathVariable Long id, @RequestBody HomeworkRequestDto requestDto, HttpServletRequest request){
-        return homeworkService.update(id,requestDto,request);
+        String token = jwtUtil.resolveToken(request);
+        Claims claims;
+        if (token != null) {
+            if (jwtUtil.validateToken(token)) {
+                claims = jwtUtil.getUserInfoFromToken(token);
+            } else {
+                throw new IllegalArgumentException("유효한 토큰이 아닙니다.");
+            }
+            String userNameToken = claims.getSubject();
+            return homeworkService.update(id,requestDto,userNameToken);
+        }
+        throw new IllegalArgumentException("로그인 해 주십시오");
     }
+
     @DeleteMapping("/post/{id}")
     public ResponseEntity<DefaultRes> deleteHomework(@PathVariable Long id, HttpServletRequest request){
-        homeworkService.delete(id,request);
-        defaultRes.setStatusCode(StatusCode.OK);
-        defaultRes.setResponseMessage(ResponseMessage.DELETE_SUCCESS);
-        return new ResponseEntity<>(defaultRes, HttpStatus.OK);
+        String token = jwtUtil.resolveToken(request);
+        Claims claims;
+        if (token != null) {
+            if (jwtUtil.validateToken(token)) {
+                claims = jwtUtil.getUserInfoFromToken(token);
+            } else {
+                throw new IllegalArgumentException("유효한 토큰이 아닙니다.");
+            }
+            String userNameToken = claims.getSubject();
+            homeworkService.delete(id,userNameToken);
+            defaultRes.setStatusCode(StatusCode.OK);
+            defaultRes.setResponseMessage(ResponseMessage.DELETE_SUCCESS);
+            return new ResponseEntity<>(defaultRes, HttpStatus.OK);
+        }
+        throw new IllegalArgumentException("로그인 해 주십시오");
     }
 }
+
