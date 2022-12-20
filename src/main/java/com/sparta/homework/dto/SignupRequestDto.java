dce20f7a732b0de4d6dc074a17449635b4dd245f
leeyoungbin <2zeroempty@gmail.com>
2022-12-20 15:25:48 +0900

    "pattern검사 dto에서 하게 수정"

--- src/main/java/com/sparta/homework/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/homework/dto/SignupRequestDto.java
@@ -1,12 +1,15 @@
 package com.sparta.homework.dto;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class SignupRequestDto {
+    @Pattern(regexp = "^[0-9a-z].{4,10}$",message = "최소 4자, 최대 10자 영어소문자와 숫자만 입력할 수 있습니다")
     private String username;
+    @Pattern(regexp = "^[A-Za-z0-9#?!@$%^&*-].{8,15}$",message = "최소 8자, 최대15자 영어 대소문자, 특수문자를 입력할 수 있습니다.")
     private String password;
     private boolean admin = false;
     private String adminToken = "";
--- src/main/java/com/sparta/homework/entity/User.java
+++ src/main/java/com/sparta/homework/entity/User.java
@@ -14,10 +14,10 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    @Pattern(regexp = "^[0-9a-z].{4,10}$",message = "최소 4자, 최대 10자 영어소문자와 숫자만 입력할 수 있습니다")
+
     @Column(nullable = false)
     private String username;
-    @Pattern(regexp = "^[A-Za-z0-9#?!@$%^&*-].{8,15}$",message = "최소 8자, 최대15자 영어 대소문자, 특수문자를 입력할 수 있습니다.")
+
     @Column(nullable = false)
     private String password;
 
