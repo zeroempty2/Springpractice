e5a47b52f62c87cc19c7fc7092e0397e332d9d54
leeyoungbin <2zeroempty@gmail.com>
2022-12-21 00:42:45 +0900

    "토큰이 유효성 검증 메서드 적용"

--- src/main/java/com/sparta/homework/controller/CommentController.java
+++ src/main/java/com/sparta/homework/controller/CommentController.java
@@ -1,12 +1,9 @@
 package com.sparta.homework.controller;
 
 import com.sparta.homework.dto.CommentRequestDto;
-import com.sparta.homework.exception.InvalidTokenException;
-import com.sparta.homework.exception.NotLoginException;
 import com.sparta.homework.jwt.JwtUtil;
 import com.sparta.homework.responseMessageData.DefaultRes;
 import com.sparta.homework.service.CommentService;
-import io.jsonwebtoken.Claims;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -27,49 +24,23 @@ public class CommentController {
     @ApiOperation(value = "댓글 작성", notes = "댓글을 작성한다.")
     public CommentRequestDto addComment(@RequestBody CommentRequestDto requestDto, HttpServletRequest request, @PathVariable Long id) {
         String token = jwtUtil.resolveToken(request);
-        Claims claims;
-        if (token != null) {
-            if (jwtUtil.validateToken(token)) {
-                claims = jwtUtil.getUserInfoFromToken(token);
-            } else {
-                throw new InvalidTokenException();
-            }
-            String userNameToken = claims.getSubject();
-            return commentService.addComment(requestDto, userNameToken, id);
+        String userNameToken = jwtUtil.isValidToken(token);
+        return commentService.addComment(requestDto, userNameToken, id);
         }
-        throw new NotLoginException();
-    }
+
     @PutMapping("/comment/{commentId}")
     @ApiOperation(value = "댓글 수정", notes = "댓글을 수정한다.")
     public CommentRequestDto updateComment(@RequestBody CommentRequestDto requestDto, HttpServletRequest request, @PathVariable Long id, @PathVariable Long commentId){
         String token = jwtUtil.resolveToken(request);
-        Claims claims;
-        if (token != null) {
-            if (jwtUtil.validateToken(token)) {
-                claims = jwtUtil.getUserInfoFromToken(token);
-            } else {
-                throw new InvalidTokenException();
-            }
-            String userNameToken = claims.getSubject();
-            return commentService.updateComment(commentId,id,requestDto,userNameToken);
+        String userNameToken = jwtUtil.isValidToken(token);
+        return commentService.updateComment(commentId,id,requestDto,userNameToken);
         }
-        throw new NotLoginException();
-    }
     @DeleteMapping("/comment/{commentId}")
     @ApiOperation(value = "댓글 삭제", notes = "댓글을 삭제한다.")
     public ResponseEntity<DefaultRes> deleteComment(HttpServletRequest request, @PathVariable Long id, @PathVariable Long commentId){
         String token = jwtUtil.resolveToken(request);
-        Claims claims;
-        if (token != null) {
-            if (jwtUtil.validateToken(token)) {
-                claims = jwtUtil.getUserInfoFromToken(token);
-            } else {
-                throw new InvalidTokenException();
-            }
-            String userNameToken = claims.getSubject();
-            commentService.deleteComment(commentId,id,userNameToken);
-            return defaultRes.deleteOK();
+        String userNameToken = jwtUtil.isValidToken(token);
+        commentService.deleteComment(commentId,id,userNameToken);
+        return defaultRes.deleteOK();
         }
-        throw new NotLoginException();
-    }
 }
--- src/main/java/com/sparta/homework/controller/PostController.java
+++ src/main/java/com/sparta/homework/controller/PostController.java
@@ -2,12 +2,9 @@ package com.sparta.homework.controller;
 
 import com.sparta.homework.dto.PostRequestDto;
 import com.sparta.homework.dto.PostResponseDto;
-import com.sparta.homework.exception.InvalidTokenException;
-import com.sparta.homework.exception.NotLoginException;
 import com.sparta.homework.jwt.JwtUtil;
 import com.sparta.homework.responseMessageData.DefaultRes;
 import com.sparta.homework.service.PostService;
-import io.jsonwebtoken.Claims;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -30,18 +27,10 @@ public class PostController {
     @ApiOperation(value = "게시글 작성", notes = "게시글을 작성한다.")
     public PostRequestDto createPost(@RequestBody PostRequestDto requestDto, HttpServletRequest request) {
         String token = jwtUtil.resolveToken(request);
-        Claims claims;
-        if (token != null) {
-            if (jwtUtil.validateToken(token)) {
-                claims = jwtUtil.getUserInfoFromToken(token);
-            } else {
-                throw new InvalidTokenException();
-            }
-            String userNameToken = claims.getSubject();
-            return postService.createPost(requestDto, userNameToken);
+        String userNameToken = jwtUtil.isValidToken(token);
+        return postService.createPost(requestDto, userNameToken);
         }
-        throw new NotLoginException();
-    }
+
     @GetMapping("/posts")
     @ApiOperation(value = "전체 게시글 불러오기", notes = "전체 게시글을 불러온다")
     public List<PostResponseDto> getPosts(){
@@ -56,34 +45,16 @@ public class PostController {
     @ApiOperation(value = "게시글 수정", notes = "선택한 게시글을 수정한다")
     public PostRequestDto updatePost(@PathVariable Long id, @RequestBody PostRequestDto requestDto, HttpServletRequest request){
         String token = jwtUtil.resolveToken(request);
-        Claims claims;
-        if (token != null) {
-            if (jwtUtil.validateToken(token)) {
-                claims = jwtUtil.getUserInfoFromToken(token);
-            } else {
-                throw new InvalidTokenException();
-            }
-            String userNameToken = claims.getSubject();
-            return postService.update(id,requestDto,userNameToken);
+        String userNameToken = jwtUtil.isValidToken(token);
+        return postService.update(id,requestDto,userNameToken);
         }
-        throw new NotLoginException();
-    }
     @DeleteMapping("/post/{id}")
     @ApiOperation(value = "게시글 삭제", notes = "선택한 게시글을 삭제한다")
     public ResponseEntity<DefaultRes> deletePost(@PathVariable Long id, HttpServletRequest request){
         String token = jwtUtil.resolveToken(request);
-        Claims claims;
-        if (token != null) {
-            if (jwtUtil.validateToken(token)) {
-                claims = jwtUtil.getUserInfoFromToken(token);
-            } else {
-                throw new InvalidTokenException();
-            }
-            String userNameToken = claims.getSubject();
-            postService.delete(id,userNameToken);
-            return defaultRes.deletePostOk();
+        String userNameToken = jwtUtil.isValidToken(token);
+        postService.delete(id,userNameToken);
+        return defaultRes.deletePostOk();
         }
-        throw new NotLoginException();
-    }
 }
 
