b9e821a3aba9b1ad83559c45ff46f95ecef6f367
leeyoungbin <2zeroempty@gmail.com>
2022-12-21 03:01:39 +0900

    "@Pattern message 변경"

--- src/main/java/com/sparta/homework/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/homework/dto/SignupRequestDto.java
@@ -11,7 +11,7 @@ import javax.validation.constraints.Pattern;
 @Setter
 public class SignupRequestDto {
     @ApiModelProperty(example = "최소 4자, 최대 10자 영어소문자와 숫자인 이름")
-    @Pattern(regexp = "^[0-9a-z].{4,10}$",message = "최소 4자, 최대 10자 영어소문자와 숫자만 입력할 수 있습니다")
+    @Pattern(regexp = "^[0-9a-z].{4,10}$",message = "최소 4자, 최대 10자 영어소문자와 숫자만 입력할 수 있습니다.")
     private String username;
     @ApiModelProperty(example = "최소 8자, 최대15자 영어 대소문자, 특수문자인 비밀번호")
     @Pattern(regexp = "^[A-Za-z0-9#?!@$%^&*-].{8,15}$",message = "최소 8자, 최대15자 영어 대소문자, 특수문자를 입력할 수 있습니다.")
