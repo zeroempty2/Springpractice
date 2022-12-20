b39ffdf1d5c68e3e04452cfa05259dac801b451f
leeyoungbin <2zeroempty@gmail.com>
2022-12-21 03:00:08 +0900

    "사용하지 않는 message삭제"

--- src/main/java/com/sparta/homework/responseMessageData/ResponseMessage.java
+++ src/main/java/com/sparta/homework/responseMessageData/ResponseMessage.java
@@ -4,12 +4,10 @@ package com.sparta.homework.responseMessageData;
 
 
 public class ResponseMessage {
-        public static final String LOGIN_SUCCESS = "로그인 성공";
-        public static final String LOGIN_FAIL = "로그인 실패";
-        public static final String NOT_FOUND_USER = "회원을 찾을 수 없습니다.";
-        public static final String CREATED_USER = "회원 가입 성공";
-        public static final String INTERNAL_SERVER_ERROR = "서버 내부 에러";
-        public static final String DELETE_SUCCESS = "삭제 성공";
-        public static final String DB_ERROR = "데이터베이스 에러";
+public static final String LOGIN_SUCCESS = "로그인 성공";
+public static final String INVALID_USERNAME_PATTERN = "최소 4자, 최대 10자 영어소문자와 숫자만 입력할 수 있습니다";
+public static final String INVALID_PASSWORD_PATTERN = "최소 8자, 최대15자 영어 대소문자, 특수문자를 입력할 수 있습니다";
+public static final String CREATED_USER = "회원 가입 성공";
+public static final String DELETE_SUCCESS = "삭제 성공";
     }
 
