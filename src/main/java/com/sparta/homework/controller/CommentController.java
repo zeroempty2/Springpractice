cd1063df214ccfca32af157dce63f7aa39e2991d
leeyoungbin <2zeroempty@gmail.com>
2022-12-21 16:39:25 +0900

    "Swagger Api설명 추가"

--- src/main/java/com/sparta/homework/controller/CommentController.java
+++ src/main/java/com/sparta/homework/controller/CommentController.java
@@ -5,6 +5,8 @@ import com.sparta.homework.jwt.JwtUtil;
 import com.sparta.homework.responseMessageData.DefaultRes;
 import com.sparta.homework.service.CommentService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +23,7 @@ public class CommentController {
     private final JwtUtil jwtUtil;
     private final DefaultRes defaultRes;
     @PostMapping("/comment")
+    @ApiImplicitParam(name = "id", value = "게시글 id")
     @ApiOperation(value = "댓글 작성", notes = "댓글을 작성한다.")
     public CommentRequestDto addComment(@RequestBody CommentRequestDto requestDto, HttpServletRequest request, @PathVariable Long id) {
         String token = jwtUtil.resolveToken(request);
@@ -29,6 +32,7 @@ public class CommentController {
         }
 
     @PutMapping("/comment/{commentId}")
+    @ApiImplicitParams({@ApiImplicitParam(name = "id", value = "게시글 id"),@ApiImplicitParam(name = "commentId", value = "댓글 id")})
     @ApiOperation(value = "댓글 수정", notes = "댓글을 수정한다.")
     public CommentRequestDto updateComment(@RequestBody CommentRequestDto requestDto, HttpServletRequest request, @PathVariable Long id, @PathVariable Long commentId){
         String token = jwtUtil.resolveToken(request);
@@ -36,6 +40,7 @@ public class CommentController {
         return commentService.updateComment(commentId,id,requestDto,userInfo);
         }
     @DeleteMapping("/comment/{commentId}")
+    @ApiImplicitParams({@ApiImplicitParam(name = "id", value = "게시글 id"),@ApiImplicitParam(name = "commentId", value = "댓글 id")})
     @ApiOperation(value = "댓글 삭제", notes = "댓글을 삭제한다.")
     public ResponseEntity<DefaultRes> deleteComment(HttpServletRequest request, @PathVariable Long id, @PathVariable Long commentId){
         String token = jwtUtil.resolveToken(request);
--- src/main/java/com/sparta/homework/controller/PostController.java
+++ src/main/java/com/sparta/homework/controller/PostController.java
@@ -6,6 +6,7 @@ import com.sparta.homework.jwt.JwtUtil;
 import com.sparta.homework.responseMessageData.DefaultRes;
 import com.sparta.homework.service.PostService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -37,11 +38,13 @@ public class PostController {
         return  postService.getPosts();
     }
     @GetMapping("/post/{id}")
+    @ApiImplicitParam(name = "id", value = "게시글 id")
     @ApiOperation(value = "게시글 불러오기", notes = "선택한 게시글을 불러온다")
     public PostResponseDto getSelectPosts(@PathVariable Long id){
         return postService.getSelectPosts(id);
     }
     @PutMapping("/post/{id}")
+    @ApiImplicitParam(name = "id", value = "게시글 id")
     @ApiOperation(value = "게시글 수정", notes = "선택한 게시글을 수정한다")
     public PostRequestDto updatePost(@PathVariable Long id, @RequestBody PostRequestDto requestDto, HttpServletRequest request){
         String token = jwtUtil.resolveToken(request);
@@ -49,6 +52,7 @@ public class PostController {
         return postService.update(id,requestDto,userInfo);
         }
     @DeleteMapping("/post/{id}")
+    @ApiImplicitParam(name = "id", value = "게시글 id")
     @ApiOperation(value = "게시글 삭제", notes = "선택한 게시글을 삭제한다")
     public ResponseEntity<DefaultRes> deletePost(@PathVariable Long id, HttpServletRequest request){
         String token = jwtUtil.resolveToken(request);
