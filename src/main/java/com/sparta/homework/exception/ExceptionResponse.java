660e63480302ad94027ba2ea636870bebe08631f
leeyoungbin <2zeroempty@gmail.com>
2022-12-21 16:59:11 +0900

    "public -> package-private"

--- src/main/java/com/sparta/homework/exception/ExceptionResponse.java
+++ src/main/java/com/sparta/homework/exception/ExceptionResponse.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 @Getter
 @Component
 public class ExceptionResponse {
-    public int statusCode;
-    public String message;
-    public boolean success;
+    int statusCode;
+    String message;
+    boolean success;
 }
