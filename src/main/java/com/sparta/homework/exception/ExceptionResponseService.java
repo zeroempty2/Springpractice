806c6c31ac96d8160a35b68740fe173fb7510545
leeyoungbin <2zeroempty@gmail.com>
2022-12-21 10:05:12 +0900

    "makeMethodArgumentNotValidExceptionResponse메서드 코드 수정"

--- src/main/java/com/sparta/homework/exception/ExceptionResponseService.java
+++ src/main/java/com/sparta/homework/exception/ExceptionResponseService.java
@@ -9,25 +9,33 @@ import java.util.Objects;
 @Service
 @RequiredArgsConstructor
 public class ExceptionResponseService {
-   private final ExceptionResponse exceptionResponse;
-    public ExceptionResponse getErrorResponse(int statusCode, String message){
+    private final ExceptionResponse exceptionResponse;
+
+    public ExceptionResponse getErrorResponse(int statusCode, String message) {
         exceptionResponse.statusCode = statusCode;
         exceptionResponse.message = message;
         exceptionResponse.success = false;
         return exceptionResponse;
     }
-    public ExceptionResponse makeMethodArgumentNotValidExceptionResponse(BindingResult bindingResult){
+
+    public ExceptionResponse makeMethodArgumentNotValidExceptionResponse(BindingResult bindingResult) {
         exceptionResponse.statusCode = 400;
         exceptionResponse.message = "지정해주지 않은 유형의 MethodArgumentNotValidException 입니다";
         exceptionResponse.success = false;
         //에러가 있다면
-        if(bindingResult.hasErrors()){
+        if (bindingResult.hasErrors()) {
             //DTO에 유효성체크를 걸어놓은 어노테이션명을 가져온다.
             String bindResultCode = Objects.requireNonNull(bindingResult.getFieldError()).getCode();
             //유형지정
-            if(Objects.requireNonNull(bindResultCode).equals("Pattern")){
-                exceptionResponse.statusCode = Exception.INVALID_PATTERN.getStatusCode();
-                exceptionResponse.message= Exception.INVALID_PATTERN.getMessage();
+            switch (Objects.requireNonNull(bindResultCode)) {
+                case "Pattern" -> {
+                    exceptionResponse.statusCode = Exception.INVALID_PATTERN.getStatusCode();
+                    exceptionResponse.message = Exception.INVALID_PATTERN.getMessage();
+                }
+                case "addCase" -> {
+                    exceptionResponse.statusCode = 400;
+                    exceptionResponse.message = "에러코드와 메세지를 작성해주세요";
+                }
             }
         }
         return exceptionResponse;
